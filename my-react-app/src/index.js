42d6f1165341598ad2e383e3f31e9282c500ee08
nafnu <21520@student.dorset-college.ie>
2021-03-10 11:08:28 +0000

    commit

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function NameLength(props){
+    return<h1>The name {props.name} contains {props.name.length} characters!</h1>;
+}
+
+function App(){
+    return(
+        <div>
+            <NameLength name = "Erika" />
+            <NameLength name = "George" />
+            <NameLength name = "Phillip" />
+        </div>
+    );
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <h1>Es hora de dormir</h1>,
+  document.getElementById('other')
+);
+
+
